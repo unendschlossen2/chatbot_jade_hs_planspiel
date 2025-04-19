60ddd100a79123cab3eec033867924fe22b2347e
Pero Juric <pero.juric@student.jade-hs.de>
2025-04-19 14:34:03 +0200

    Delete Version_5_Ollama directory
    Version 5 in new branch

--- Version_5_Ollama/src/embedding/huggingface_login.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import login
-
-def huggingface_hub_login(token: str):
-    login(token)
-    print("Logged in to Hugging Face Hub successfully.")
-    return True
--- Version_5_Ollama/src/main.py
+++
@@ -1,4 +0,0 @@
-from interface import web_ui
-
-if __name__ == "__main__":
-    web_ui.generate_window()
